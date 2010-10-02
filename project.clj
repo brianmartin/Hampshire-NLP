44d1abe43192fcd8b0ef3ffabb750488aa00ce76
Brian Martin <brianmartin@gmail.com>
2010-10-02 13:50:50 -0400

    base parsing work (no coref), main loop not yet working

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Clojure wrapper for the Stanford Parser"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [edu.stanford.nlp/stanford-parser "1.6.2"]])
+                 [org.clojars.pjt/opennlp-tools "1.4.3"]
+                 [edu.stanford.nlp/stanford-parser "1.6.2"]]
+  :main stanford-parser-clj.core)
--- src/stanford_parser_clj/core.clj
+++ src/stanford_parser_clj/core.clj
@@ -1,1 +1,87 @@
-(ns stanford-parser-clj.core)
+(ns stanford-parser-clj.core
+  (:use [clojure.contrib.duck-streams :as d]
+        [clojure.contrib.command-line])
+  (:import [java.io PrintWriter StringWriter]
+           [edu.stanford.nlp.process DocumentPreprocessor]
+           [edu.stanford.nlp.parser.lexparser LexicalizedParser]
+           [edu.stanford.nlp.trees TreePrint]
+           [edu.stanford.nlp.ling Word HasWord]
+           [opennlp.tools.coref DefaultLinker DiscourseEntity Linker LinkerMode]
+           [opennlp.tools.coref.mention DefaultParse Mention MentionContext PTBMentionFinder]
+           [opennlp.tools.parser Parse]
+           [opennlp.tools.parser.chunking Parser]
+           [opennlp.tools.util Span]
+           [opennlp.tools.lang.english TreebankLinker]))
+
+(defn file-to-parses
+  [file charset lp dp]
+  (with-open [rdr (reader file)]
+    (let [sentences (. dp getSentencesFromText rdr)]
+      (doall
+        (for [s sentences]
+          (do 
+            (. lp parse s)
+            (. lp getBestParse)))))))
+
+(defn parse-to-treebank-string
+  [p]
+  (let [sw (StringWriter.)
+        tp (TreePrint. "penn")]
+    (. tp printTree p (PrintWriter. sw))
+    (. sw toString)))
+
+(defn parses-to-treebank-strings
+  [parses]
+  (map parse-to-treebank-string parses))
+
+(defn parses-to-dep-strings
+  [parses]
+  (let [tp (TreePrint. "typedDependenciesCollapsed")]
+    (doall 
+      (for [t parses]
+        (let [sw (StringWriter.)]
+          (. tp printTree t (PrintWriter. sw))
+          (. sw toString))))))
+
+(defn -main [& args]
+  (with-command-line args "Parse and Coref"
+    [[input-dir i "Folder containing input files." "~/test-data"]
+     [output-dir o "Destination folder for output files." "~/output-data"]
+     [charset c "Charset of input." "utf-8"]
+     [grammar g "Grammar file for Stanford Parser." "data/englishPCFG.ser.gz"]
+     etc]
+
+  (let [lp (LexicalizedParser. grammar)
+        dp (DocumentPreprocessor.)
+        idir (d/file-str input-dir)
+        files (. idir listFiles)
+        file-cnt (count files)
+        odir (d/file-str output-dir)
+        parses
+          (loop [data [] fs files cnt 1]
+            (if (seq fs)
+              (do 
+                (println "Initial parse of file: " cnt " / " file-cnt)
+                (recur (conj data (file-to-parses (first fs) charset lp dp))
+                       (rest fs)
+                       (inc cnt)))
+              (println "done1")))
+        stanford-dep-parses 
+          (loop [data [] ps parses cnt 1]
+            (if (seq ps)
+              (do
+                (println "Stanford parse of file: " cnt " / " file-cnt)
+                (println data)
+                (recur (conj data (parses-to-dep-strings (first ps)))
+                       (rest ps)
+                       (inc cnt)))
+              (println "done2")))
+        stringed-parses
+          (loop [data [] ps parses cnt 1]
+            (if (seq ps)
+              (do
+                (println "Stringing parse of file: " cnt " / " file-cnt)
+                (recur (conj data (parses-to-treebank-strings (first ps)))
+                       (rest ps)
+                       (inc cnt)))))]
+    stringed-parses)))
