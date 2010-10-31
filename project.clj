831aafbb846daa10b0d1119d19b33c1e097947f1
Brian Martin <brianmartin@gmail.com>
2010-10-31 16:32:16 -0400

    now using opennlp 1.5.0 vs. 1.4.3 and jvm opts to increase heap size

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject narrative-chains "0.0.1"
-  :description "Clojure wrapper for the Stanford Parser (distributed using rabbitmq)."
+  :description "Narrative Chains Model (distributed using rabbitmq)."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.pjt/opennlp-tools "1.4.3"]
+                 [org.clojars.zaxtax/opennlp-tools "1.5.0"]
                  [edu.stanford.nlp/stanford-parser "1.6.2"]
                  [org.clojars.adityo/clj-tagsoup "0.1.2"]
                  [clj-ssh "0.2.0-SNAPSHOT"]
                  [rabbitcj "0.1.0-SNAPSHOT"]]
-  :main narrative-chains.core)
+  :main narrative-chains.core
+  :java-opts ["-server" "-Xms3g" "-Xmx5g"])
