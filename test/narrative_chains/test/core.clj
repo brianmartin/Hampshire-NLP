8ac0170d7c0203683552adfd40579cc22409030d
Brian Martin <brianmartin@gmail.com>
2010-12-20 17:54:50 -0500

    removed tests (not used)

--- test/narrative_chains/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns narrative-chains.test.core
-  (:use [stanford-parser-clj.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
